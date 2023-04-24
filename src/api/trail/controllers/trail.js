1d59b2d0cf9ed5ac07fa16928834a4f9e94db90c
Hector Heriberto  Rodriguez Martinez <soybisonte@MacBook-Pro-de-Hector.local>
2023-04-24 13:06:59 -0600

    Initial commit

---
+++ src/api/trail/controllers/trail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trail.trail');
---
+++ src/api/trail/services/trail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::trail.trail');
